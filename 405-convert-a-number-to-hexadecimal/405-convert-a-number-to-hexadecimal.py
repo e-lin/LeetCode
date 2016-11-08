a8626d66440b4564668d112aa9a3b631bfe5133d
Elsa <e-lin@groovenauts.jp>
2016-11-08 18:21:52 +0900

    fix 405

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
@@ -1,32 +1,40 @@
+# Solution Reference:
+# http://stackoverflow.com/questions/16427073/signed-integer-to-twos-complement-hexadecimal
+
 class Solution(object):
     def toHex(self, num):
         """
         :type num: int
         :rtype: str
         """
-        d = { 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f' }
-        l = list(bin(num)[2:])
-        l = l[::-1]
-        l.extend('0' * (4*(len(l)/4+1) - len(l)))
+        #### works with positive num but fails with negative num
+        #
+        # d = { 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f' }
+        # l = list(bin(num)[2:])
+        # l = l[::-1]
+        # l.extend('0' * (4*(len(l)/4+1) - len(l)))
+        #
+        # group = 0
+        # hex_result = ""
+        # temp = 0
+        #
+        # for i in xrange(len(l)):
+        #     temp += (int(l[i]) * (2**group))
+        #
+        #     if group == 3:  # start from 0 and every 4 is a group
+        #         if temp < 10:
+        #             hex_result += str(temp)
+        #         else:
+        #             hex_result += str(d[temp])
+        #         temp = 0
+        #         group = 0
+        #     else:
+        #         group += 1
+        #
+        # return hex_result[::-1]
 
-        group = 0
-        hex_result = ""
-        temp = 0
-
-        for i in xrange(len(l)):
-            temp += (int(l[i]) * (2**group))
-
-            if group == 3:  # start from 0 and every 4 is a group
-                if temp < 10:
-                    hex_result += str(temp)
-                else:
-                    hex_result += str(d[temp])
-                temp = 0
-                group = 0
-            else:
-                group += 1
-
-        return hex_result[::-1]
+        bits = 32
+        return "{0:{1}x}".format(num & ((1<<bits) - 1), bits//4).lstrip()
 
 
 def main():
